1116a49a8bcad21bdd1c37ae661afe7d2fced2b6
sudoshivam <sy76076@gmail.com>
2021-06-27 03:16:09 +0530

    first commit

--- karngyan.config.js
+++ karngyan.config.js
@@ -1,10 +1,10 @@
 // Upload files in static/ So any file static/file.pdf -> /file.pdf is accessible at root
 
 export default {
-  name: 'gyan prakash karn',
-  domain: 'template.karngyan.com', // add without https:// , used in meta tags and share urls
-  image: '/images/bigheadkarngyan.png',
-  email: 'mail@karngyan.com',
+  name: 'shivam yadav',
+  domain: 'sudoshivam.tech', // add without https:// , used in meta tags and share urls
+  image: '/images/profile.png',
+  email: 'sy76076@gmail.com',
   googleAnalyticsV4: {
     enabled: false,
     id: ''
@@ -19,53 +19,52 @@ export default {
   // enable if you want comments and likes on posts
   // see how it looks on karngyan.com
   firebase: {
-    enabled: true
+    enabled: false
   },
   social: {
-    github: 'karngyan',
-    linkedin: 'karngyan',
-    facebook: 'karnsometimes',
-    twitter: 'gyankarn',
-    instagram: 'karngyan.dev',
-    codestats: 'karngyan' // https://codestats.net make a profile if you dont already have one.
+    github: 'sudoshivam',
+    linkedin: 'sudoshivam',
+    facebook: 'sudoshivam',
+    twitter: 'sudoshivam',
+    instagram: 'sudoshivam',
+    codestats: 'sudoshivam' // https://codestats.net make a profile if you dont already have one.
   },
   buyMeACoffee: {
-    enabled: true,
+    enabled: false,
     url: 'https://www.buymeacoffee.com/karngyan'
   },
   projects: {
     enabled: true,
   },
   blog: {
-    enabled: true,
+    enabled: false,
   },
   resume: {
-    enabled: true,
+    enabled: false,
     pdfUrl: '/RESUME_GYAN_KARN_DARK.pdf' // add files in static folder
   },
   uses: {
     enabled: true,
     meta: [
-      {title: 'OS', value: 'macOS Catalina'},
-      {title: 'Memory', value: '16 GB 2667 MHz DDR4'},
-      {title: 'Keyboard', value: 'Keychron K2 - Gateron Brown Keys'},
-      {title: 'Mouse', value: 'Logitech Silent Pebble'},
-      {title: 'Monitor', value: 'LG QHD (2560 x 1440) 27 Inch IPS Display'},
-      {title: 'Laptop • Processor • Graphics', value: 'MacBook Pro (16-inch, 2019) • 2.6 GHz 6-Core Intel Core i7 • AMD Radeon Pro 5300M 4 GB + Intel UHD Graphics 630 1536 MB'}
+      {title: 'OS', value: 'Windows 10 Pro 64-bit'},
+      {title: 'Memory', value: '8.00 GB'},
+      {title: 'Processor', value: 'AMD Ryzen 5 3550H'},
+      {title: 'Graphics', value: '4095MB NVIDIA GeForce GTX 1650'},
+      {title: 'Monitor', value: 'LG QHD (2560 x 1440) 27 Inch IPS Display'}
     ]
   },
   workedAt: {
     // add logos in static and at max add 3/4
     enabled: true,
     meta: [
-      { name: 'SendPost', src: '/images/sendpost.png', url: 'https://sendpost.io' },
-      { name: 'Amazon', src: '/images/amazon.png', url: 'https://amazon.in' },
-      { name: 'InterviewReady', src: '/images/interviewready-io.png', url: 'https://get.interviewready.io' },
+      { name: 'CS50', src: '/images/csfifty.png', url: 'https://cs50.harvard.edu' },
+      { name: 'Opensource Dev', src: '/images/github.png', url: 'https://github.com' },
+      { name: 'Fiverr', src: '/images/fiverr.png', url: 'https://fiverr.com' },
       { name: 'CrioDo', src: '/images/crio.png', url: 'https://crio.do' },
     ]
   },
   loadingIndicator: {
-    name: 'pulse'
+    name: 'folding-cube'
     // https://tobiasahlin.com/spinkit/
     // circle
     // cube-grid
@@ -96,8 +95,8 @@ export default {
         iBlogTech: 'i blog tech',
         haveALook: 'have a look',
         friendlyNeighborhood: 'your friendly neighborhood',
-        description: 'i am a software engineer and a part time freelancer. i blog tech, write a weekend newsletter called software shots, and tinker with side projects every now n then.',
-        words: ['developer', 'designer', 'engineer', 'programmer', 'encoder'],
+        description: 'i am a web developer and a part time freelancer. i love scripting, graphic design and talking about tech in online communities.',
+        words: ['developer', 'designer', 'engineer', 'programmer', 'artist'],
       },
       githubCalendar: {
         header: 'contributions',
@@ -105,7 +104,7 @@ export default {
       },
       blog: {
         header: 'blog',
-        subtext: 'i try to write once in a while. let me know your thoughts in comments or mail@karngyan.com'
+        subtext: 'i try to write once in a while. let me know your thoughts in comments or mail me at sy76076@gmail.com'
       },
       recentBlog: {
         header: 'recent blogs',
@@ -117,7 +116,7 @@ export default {
       },
       projects: {
         header: 'projects',
-        subtext: 'this page lists some of my personal and work projects. every project has some story, click on the title to read'
+        subtext: 'this page lists some of my personal and work projects.'
       }
     }
   }
